eefec04567b66034bdcdb1dd51b8978437ac01d2
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-29 16:40:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,16 +22,3 @@ end
 fido = Dog.new
 fido.breed = "Beagle" 
 
-#   def initialize(name)
-#     @name = name
-#   end
- 
-#   def name
-#     @name
-#   end
- 
-#   def name=(new_name)
-#     @name = new_name
-#   end
- 
-# end
