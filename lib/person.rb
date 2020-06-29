2be5413593f9e85396dce33f36a65330c304c41f
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-29 16:42:15 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,41 @@
+class Person
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(persons_job)
+    @job = persons_job
+  end
+  
+  def job
+    @job
+  end
+  
+end
+
+
+
+
+
+
+
+
+
+#   def initialize(name)
+#     @name = name
+#   end
+ 
+#   def name
+#     @name
+#   end
+ 
+#   def name=(new_name)
+#     @name = new_name
+#   end
+ 
+# end
